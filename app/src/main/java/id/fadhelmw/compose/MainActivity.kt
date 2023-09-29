8a293ff9dda76afc0e615ab071332a252de464ca
mwfadhel <aizenapple@gmail.com>
2023-09-29 14:44:21 +0700

    membuat tampilan profile

--- app/src/main/java/id/fadhelmw/compose/MainActivity.kt
+++ app/src/main/java/id/fadhelmw/compose/MainActivity.kt
@@ -1,87 +1,152 @@
-// nama package yang digunakan
 package id.fadhelmw.compose
-//import package
-import androidx.compose.foundation.border
-import androidx.compose.material.MaterialTheme
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import id.fadhelmw.compose.ui.theme.ComposeTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import id.fadhelmw.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.tooling.preview.Preview
 
-
+data class ContactInfo(
+    val name: String,
+    val title: String,
+    val email: String,
+    val imageResource: Int // Gunakan resource gambar lokal
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
-                }
+            ComposeTheme {
+                // Define your contact list here
+                val contactList = listOf(
+                    ContactInfo("Fadhel Muhammad W", "Full Stack Engineering", "mw.fadhel@google.com", R.drawable._75874270_513287),
+                    // Add more contacts as needed
+                )
+
+                ContactList(contactList = contactList)
             }
         }
     }
 }
-//mendeklarasikan class Message
-data class Message(val author : String, val body: String)
-// fungsi composable memanggil 2 elemen text dengan menambahkan fungsi column
-// sehingga text tidak tertumpuk
+
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+fun ContactCard(contactInfo: ContactInfo) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(16.dp)
+    ) {
+        // Foto (dalam bentuk bulat dan berada di tengah)
+        Box(
             modifier = Modifier
-                // Set gambar > 40 dp
-                .size(40.dp)
-                // Memotong gambar menjadi bundar
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .size(96.dp)
+                .background(Color.Gray, shape = CircleShape)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Image(
+                painter = painterResource(id = contactInfo.imageResource),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp) // Ubah ukuran foto sesuai kebutuhan
+                    .clip(CircleShape)
+            )
+        }
+
+        // Nama dan Jabatan
+        Column(
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(
+                text = contactInfo.name,
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Text(
+                text = contactInfo.title,
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                color = Color.Gray
+            )
+        }
+
+        // Email
+        Text(
+            text = contactInfo.email,
+            fontSize = 16.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 8.dp)
         )
 
-        // menambahkan jarak antara gambar dan kolom
-        Spacer(modifier = Modifier.width(8.dp))
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
 
-        Column {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant
+@Composable
+fun ContactInfoSection(email: String) {
+    Spacer(modifier = Modifier.height(16.dp))
+
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = null,
+                tint = Color.Gray,
+                modifier = Modifier.size(24.dp)
             )
-            // menambahkan jara antara author dan text
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = email,
+                fontSize = 16.sp,
+                color = Color.Black
+            )
         }
     }
 }
-    // fungsi composable untuk menampilkan nama yang telah ditentukan.
-    @Preview(showBackground = true)
-    @Composable
-    fun PreviewMessageCard() {
-        ComposeTutorialTheme {
-            Surface {
-                MessageCard(
-                    msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
-                )
-            }
+
+@Composable
+fun ContactList(contactList: List<ContactInfo>) {
+    LazyColumn {
+        items(contactList) { contactInfo ->
+            ContactCard(contactInfo = contactInfo)
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
+}
+@Preview
+@Composable
+fun Profile() {
+    val contactInfo = ContactInfo("Fadhel Muhammad W", "Full Stack Engineering", "mw.fadhel@google.com", R.drawable._75874270_513287)
+    ContactCard(contactInfo = contactInfo)
+}
